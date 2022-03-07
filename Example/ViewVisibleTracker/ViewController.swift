70a655e3423bc14e1fb147ccdf72bf3212140153
david <h2s1880@gmail.com>
2022-03-07 19:13:47 +0900

    add ViewVisibleTracker

--- Example/ViewVisibleTracker/ViewController.swift
+++ Example/ViewVisibleTracker/ViewController.swift
@@ -7,18 +7,140 @@
 //
 
 import UIKit
+import ViewVisibleTracker
+
+final class TestCell: UICollectionViewCell {
+  
+  static let identifier = "TestCell"
+  
+  private let label = UILabel()
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    self.contentView.addSubview(self.label)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    self.label.text = nil
+  }
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    self.label.sizeToFit()
+    self.label.center = self.contentView.center
+  }
+  
+  func configure(indexPath: IndexPath) {
+    self.label.text = "\(indexPath.item)"
+    self.label.font = UIFont.boldSystemFont(ofSize: 36.0)
+    self.label.textColor = UIColor.white
+    if indexPath.item % 2 == 0 {
+      self.backgroundColor = UIColor.gray
+    } else {
+      self.backgroundColor = UIColor.darkGray
+    }
+  }
+}
 
 class ViewController: UIViewController {
+  
+  private let collectionView: UICollectionView = {
+    let layout = UICollectionViewFlowLayout()
+    layout.scrollDirection = .vertical
+    layout.minimumLineSpacing = 0.0
+    layout.minimumInteritemSpacing = 0.0
+    let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
+    view.backgroundColor = .white
+    view.register(TestCell.self, forCellWithReuseIdentifier: TestCell.identifier)
+    return view
+  }()
+  
+  private let viewVisibleTracker = ViewVisibleTracker()
+  
+  override func loadView() {
+    self.view = self.collectionView
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.viewVisibleTracker.delegate = self
+    self.viewVisibleTracker.dataSource = self
+    self.viewVisibleTracker.scrollView = self.collectionView
+    self.collectionView.delegate = self
+    self.collectionView.dataSource = self
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    self.trackingVisibleCellsIfNeeded()
+  }
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+extension ViewController: UICollectionViewDataSource {
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 100
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TestCell.identifier, for: indexPath) as? TestCell else {
+      fatalError()
     }
+    cell.configure(indexPath: indexPath)
+    return cell
+  }
+  
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension ViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    return CGSize(width: collectionView.bounds.width, height: 300.0)
+  }
+  
+  func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    self.trackingVisibleCellsIfNeeded()
+  }
+  
+  private func trackingVisibleCellsIfNeeded() {
+    let trackingItems = self.collectionView.visibleCells.compactMap { cell -> ViewVisibleTrackingItem? in
+      guard let indexPath = self.collectionView.indexPath(for: cell) else { return nil }
+      return ViewVisibleTrackingItem(
+        id: .indexPath(indexPath),
+        view: cell
+      )
     }
+    self.viewVisibleTracker.calculateItemsIfNeeded(items: trackingItems)
+  }
 
 }
 
+
+extension ViewController: ViewVisibleTrackerDataSource {
+  
+  func trackingRect(_ tracker: ViewVisibleTracker) -> CGRect {
+    return self.collectionView.frame
+  }
+  
+  func visibleRatioForItem(_ tracker: ViewVisibleTracker, item: ViewVisibleTrackingItem) -> CGFloat {
+    return 0.5
+  }
+}
+
+extension ViewController: ViewVisibleTrackerDelegate {
+  
+  func willBeginTracking(_ tracker: ViewVisibleTracker, item: ViewVisibleTrackingItem) {
+    
+    print("🚀 begin tracking: \(item.debugDescription)")
+  }
+  
+  func didEndTracking(_ tracker: ViewVisibleTracker, item: ViewVisibleTrackingItem) {
+    
+    print("👋 end tracking: \(item.debugDescription)")
+  }
+}
---
+++ Sources/ViewVisibleTracker/ViewVisibleTracker.swift
@@ -0,0 +1,184 @@
+//
+//  ViewVisibleTracker.swift
+//  ViewVisibleTracker
+//
+//  Created by david on 2022/03/07.
+//
+
+import Foundation
+import UIKit
+
+public final class ViewVisibleTracker {
+  
+  public weak var delegate: ViewVisibleTrackerDelegate?
+  public weak var dataSource: ViewVisibleTrackerDataSource?
+  public weak var scrollView: UIScrollView?
+  
+  public var defaultVisibleRatio: CGFloat = 0.0
+  public var defaultScrollDirection: ViewVisibleTrackerScrollDirection = .up
+  
+  private var cachedItems: Set<ViewVisibleTrackingItem> = .init()
+  
+  public init() {
+    
+  }
+  
+  public convenience init(scrollView: UIScrollView) {
+    self.init()
+    self.scrollView = scrollView
+  }
+  
+  public convenience init(
+    scrollView: UIScrollView,
+    dataSource: ViewVisibleTrackerDataSource,
+    delegate: ViewVisibleTrackerDelegate
+  ) {
+    self.init()
+    self.scrollView = scrollView
+    self.dataSource = dataSource
+    self.delegate = delegate
+  }
+  
+  public func calculateItemsIfNeeded(items: [ViewVisibleTrackingItem]) {
+    self.updateCachedItemsIfNeeded(items: items)
+    let endTrackingItems = self.cachedItems.subtracting(items)
+    self.removeEndTrackingItemsOnCache(items: endTrackingItems)
+    self.checkBeginTrackingItems(items: self.cachedItems)
+    self.didEndTracking(items: endTrackingItems)
+  }
+  
+  private func removeEndTrackingItemsOnCache(items: Set<ViewVisibleTrackingItem>) {
+    items.forEach { item in
+      self.cachedItems.remove(item)
+    }
+  }
+  
+  private func updateCachedItemsIfNeeded(items: [ViewVisibleTrackingItem]) {
+    items.forEach { item in
+      if var cachedItem = self.cachedItems.first(where: { $0.hashValue == item.hashValue }) {
+        cachedItem.frameInWindow = item.frameInWindow
+        self.cachedItems.update(with: cachedItem)
+      } else {
+        self.cachedItems.insert(item)
+      }
+    }
+  }
+  
+  private func checkBeginTrackingItems(items: Set<ViewVisibleTrackingItem>) {
+    items.forEach { item in
+      guard !item.isTracked,
+            let trackingItemPoint = self.trackingItemPoint(item: item),
+            self.calculatedTrackingRect(item: item).contains(trackingItemPoint)
+      else {
+        return
+      }
+      
+      var mutableItem = item
+      mutableItem.isTracked = true
+      self.cachedItems.update(with: mutableItem)
+      self.delegate?.willBeginTracking(self, item: item)
+    }
+  }
+  
+  private func didEndTracking(items: Set<ViewVisibleTrackingItem>) {
+    items.forEach {
+      self.delegate?.didEndTracking(self, item: $0)
+    }
+  }
+  
+  private func trackingItemPoint(item: ViewVisibleTrackingItem) -> CGPoint? {
+    guard let direction = self.scrollDrection() else { return nil }
+    switch direction {
+    case .up:
+      return CGPoint(
+        x: item.frameInWindow.origin.x,
+        y: item.frameInWindow.origin.y
+      )
+    case .down:
+      return CGPoint(
+        x: item.frameInWindow.origin.x,
+        y: item.frameInWindow.origin.y + item.frameInWindow.height
+      )
+    case .left:
+      return CGPoint(
+        x: item.frameInWindow.origin.x,
+        y: item.frameInWindow.origin.y
+      )
+    case .right:
+      return CGPoint(
+        x: item.frameInWindow.origin.x + item.frameInWindow.width,
+        y: item.frameInWindow.origin.y
+      )
+    }
+  }
+  
+  private func calculatedTrackingRect(item: ViewVisibleTrackingItem) -> CGRect {
+    guard let trackingRect = self.dataSource?.trackingRect(self) else {
+      assertionFailure("You must inherit ViewVisibleTrackerDataSource")
+      return .zero
+    }
+    
+    guard let scrollDirection = self.scrollDrection() else {
+      return .zero
+    }
+    
+    let ratio = self.dataSource?.visibleRatioForItem(self, item: item) ?? self.defaultVisibleRatio
+    
+    switch scrollDirection {
+    case .up:
+      let height = item.frameInWindow.height * ratio
+      return CGRect(
+        x: trackingRect.origin.x,
+        y: trackingRect.origin.y,
+        width: trackingRect.width,
+        height: trackingRect.height - height
+      )
+    case .down:
+      let height = item.frameInWindow.height * ratio
+      return CGRect(
+        x: trackingRect.origin.x,
+        y: trackingRect.origin.y + height,
+        width: trackingRect.width,
+        height: trackingRect.height - height
+      )
+    case .left:
+      let width = item.frameInWindow.width * ratio
+      return CGRect(
+        x: trackingRect.origin.x,
+        y: trackingRect.origin.y,
+        width: trackingRect.width - width,
+        height: trackingRect.height
+      )
+    case .right:
+      let width = item.frameInWindow.width * ratio
+      return CGRect(
+        x: trackingRect.origin.x + width,
+        y: trackingRect.origin.y,
+        width: trackingRect.width - width,
+        height: trackingRect.height
+      )
+    }
+  }
+  
+  private func scrollDrection() -> ViewVisibleTrackerScrollDirection? {
+    guard let velocity = self.scrollView?.panGestureRecognizer.velocity(in: nil) else {
+      assertionFailure("scrollView must not be null")
+      return nil
+    }
+    
+    if velocity.x == 0.0 && velocity.y == 0.0 {
+      return self.defaultScrollDirection
+    } else if velocity.x == 0.0 && velocity.y < 0.0 {
+      return .up
+    } else if velocity.x == 0.0 && velocity.y > 0.0 {
+      return .down
+    } else if velocity.x < 0.0 && velocity.y == 0.0 {
+      return .right
+    } else if velocity.x > 0.0 && velocity.y == 0.0 {
+      return .left
+    }
+    
+    // unsupported diagonal scroll tracking
+    return nil
+  }
+}
