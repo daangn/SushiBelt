362d64c049b4bc3c13a157739064741442381ce5
david <h2s1880@gmail.com>
2022-03-08 14:27:41 +0900

    add ViewVisibleTrackingItemStatus

---
+++ Sources/ViewVisibleTracker/ViewVisibleTrackingItemStatus.swift
@@ -0,0 +1,13 @@
+//
+//  ViewVisibleTrackingItemStatus.swift
+//  ViewVisibleTracker
+//
+//  Created by david on 2022/03/08.
+//
+
+import Foundation
+
+public enum ViewVisibleTrackingItemStatus {
+  case tracked
+  case tracking
+}
