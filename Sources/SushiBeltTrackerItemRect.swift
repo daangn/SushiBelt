f6e8e141746110ed107a637f1684a27e817e11e8
Jaxtyn <jaxtyn@daangn.com>
2022-08-16 19:05:17 +0900

    Apply code review, rect covenience init

--- Sources/SushiBeltTrackerItemRect.swift
+++ Sources/SushiBeltTrackerItemRect.swift
@@ -11,22 +11,42 @@ import Foundation
 
 import UIKit
 
-typealias PlatformView = UIView
+public typealias PlatformView = UIView
 
 #else
 
 import AppKit
 
-typealias PlatformView = NSView
+public typealias PlatformView = NSView
 
 #endif
 
 public struct SushiBeltTrackerItemRect {
   let frameInWindow: CGRect
+
+  public init(frame: CGRect) {
+    self.frameInWindow = frame
+  }
+
+  public init(origin: CGPoint, size: CGSize) {
+    self.frameInWindow = .init(origin: origin, size: size)
+  }
+
+  public init(x: CGFloat, y: CGFloat, width: CGFloat, height: CGFloat) {
+    self.frameInWindow = .init(x: x, y: y, width: width, height: height)
+  }
+
+  public init(x: Double, y: Double, width: Double, height: Double) {
+    self.frameInWindow = .init(x: x, y: y, width: width, height: height)
+  }
+
+  public init(x: Int, y: Int, width: Int, height: Int) {
+    self.frameInWindow = .init(x: x, y: y, width: width, height: height)
+  }
 }
 
 public extension PlatformView {
   func sushiBeltTrackerItemRect() -> SushiBeltTrackerItemRect {
-    return .init(frameInWindow: self.convert(self.bounds, to: nil))
+    return .init(frame: self.convert(self.bounds, to: nil))
   }
 }
